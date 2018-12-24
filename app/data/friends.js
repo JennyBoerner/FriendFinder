7123442cddb4a81a5c9a05c7d7e73f886c7b32b7
Jenny Boerner <jennyboerner1@gmail.com>
2018-12-23 18:37:09 -0600

    Upload

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://pbs.twimg.com/profile_images/981288787660689408/ORCJwxJV_400x400.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Susan",
+        "photo":"https://susanstiffelman.com/wp-content/uploads/2016/08/Susan-head-shot.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            1,
+            1,
+            1,
+            2,
+            3,
+            3,
+            5
+          ]
+      },
+];
+
+module.exports = friendsArray;
