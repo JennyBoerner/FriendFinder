7340ed56a6c6060e5007c68f3d826916d44c1db7
Jenny Boerner <jennyboerner1@gmail.com>
2018-12-27 16:00:41 -0600

    Update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,19 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req,res) {
-        friends.push(req.body);
+        var newFriend = req.body;
+        console.log(newFriend);
+        friends.push(newFriend);
+        res.json(newFriend);
+        
+        // Get the survey data: req.body
+        // For each friend
+        // Calculate the difference between their survey response score and ours
+        // Keep a running sum of the total difference
+        // if the total difference for the current friend is less than the lowest difference
+        // must be a new best friend
+        // set best friend attributes, name, photo, score, etc
+        // response with friend data response.json(object of values of best friend)
+        // friends.push(req.body);
     })
 }
